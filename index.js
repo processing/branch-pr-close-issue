70c89591a0230a05678d28286bbf45b03797e255
limzykenneth <limzy.kenneth@gmail.com>
2025-09-01 19:05:21 +0100

    Add additional checks in action script

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 import core from '@actions/core';
 import github from '@actions/github';
 
-try{
-  const context = github.context;
-  const prBody = context.payload.pull_request.body ?? '';
-  // This only covers linking
-  const issueRegex = /\b(?:close[sd]?|fix(?:e[sd])?|resolve[sd]?):?\s#(?<issue>\d+)\b/gmi;
+const context = github.context;
+const prBody = context.payload.pull_request.body ?? '';
+const baseBranch = context.payload.pull_request.label ?? null;
+const targetBaseBranchPattern = new RegExp(core.getInput('branch', { required: true }));
 
-  let match;
-  while ((match = issueRegex.exec(prBody)) !== null) {
-    const issueNumber = parseInt(match.groups.issue);
-    const token = core.getInput('token', { required: true });
-    const octokit = github.getOctokit(token);
+if(context.payload.pull_request.merged){
+  console.log('Pull request not merged, skipping...');
 
-    await octokit.rest.issues.update({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      issue_number: issueNumber,
-      state: 'closed',
-      state_reason: 'completed'
-    })
+}else if(targetBaseBranchPattern.test(baseBranch)){
+  console.log('Not targeted base branch, skipping...');
+
+}else{
+  try{
+    // This only covers linking
+    const issueRegex = /\b(?:close[sd]?|fix(?:e[sd])?|resolve[sd]?):?\s#(?<issue>\d+)\b/gmi;
+
+    let match;
+    while ((match = issueRegex.exec(prBody)) !== null) {
+      const issueNumber = parseInt(match.groups.issue);
+      const token = core.getInput('token', { required: true });
+      const octokit = github.getOctokit(token);
+
+      await octokit.rest.issues.update({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: issueNumber,
+        state: 'closed',
+        state_reason: 'completed'
+      })
+    }
+
+  } catch(err) {
+    console.error(err);
+    core.setFailed(err?.message ?? 'Unknown error occurred');
   }
-} catch(err) {
-  console.error(err);
-  core.setFailed(err?.message ?? 'Unknown error occurred');
 }
