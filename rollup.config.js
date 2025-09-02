f4530c15ef78f7ae2e32efcfa4e3407b2c38516e
limzykenneth <limzy.kenneth@gmail.com>
2025-09-02 20:25:01 +0100

    Build bundled script for action use

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+	input: 'index.js',
+	output: {
+		file: 'dist/index.js',
+		format: 'es'
+	},
+	plugins: [commonjs(), nodeResolve()]
+};
